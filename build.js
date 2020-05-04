65a124414b81fc3b26bea189ce4ddf5135ee4e43
Amir hossein Karimi <amir.hossein7464@gmail.com>
2020-05-04 15:44:16 +0430

    Clean build script

--- build.js
+++ build.js
@@ -1,53 +1,19 @@
 const fs = require("fs");
-const inquirer = require("inquirer");
 const lineReader = require("line-reader");
 const rimraf = require("rimraf");
 
 const mdiFilePath = require.resolve("@mdi/js/mdi.js");
 const buildDir = "./icons";
 
-run = () => {
+build = () => {
   console.log("----------------------------------");
   console.log("[mdi-js] Building icons files");
 
-  if (!fs.existsSync(buildDir)) {
-    build();
-    return;
+  if (fs.existsSync(buildDir)) {
+    console.log("Deleting old icons...");
+    rimraf.sync(buildDir);
   }
 
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "directory",
-        message: `\nBuild(${buildDir}) directory already exist.`,
-        choices: [
-          "Rename old directory (recommended)",
-          "Delete old directory!",
-        ],
-      },
-    ])
-    .then(({ directory }) => {
-      if (directory === "Rename old directory (recommended)") {
-        var d = new Date();
-        var datestring = [
-          d.getFullYear(),
-          d.getMonth() + 1,
-          d.getDate(),
-          d.getHours(),
-          d.getMinutes(),
-          d.getSeconds(),
-        ].join("-");
-        fs.renameSync(buildDir, buildDir + "-old-" + datestring);
-      } else if (directory === "Delete old directory!") {
-        rimraf.sync(buildDir);
-      }
-
-      build();
-    });
-};
-
-build = () => {
   console.log("Generating material design icons files...");
 
   fs.mkdirSync(buildDir);
@@ -72,4 +38,4 @@ build = () => {
   );
 };
 
-run();
+build();
